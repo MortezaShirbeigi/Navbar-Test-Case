615a6156325c1ab5c725f702eb717ada6add6517
Morteza Shirbeigi <shirbeigi.morteza@gmail.com>
2021-08-27 12:43:27 +0430

    feat(header): add navigation icon with animation

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import Logo from "../../assets/images/logo.svg";
+import NavigationMenu from "./navigationMenu";
 import Profile from "./profile";
 
 const Header = () => {
   return (
-    <header>
+    <header className="d-flex align-items-center header">
       <Container fluid>
         <Row>
           <Col xs={3}>
@@ -16,8 +17,8 @@ const Header = () => {
           </Col>
           <Col xs={2}>
             <div className="d-flex justify-content-end">
-              <img className="img-fluid" width="80px" src={Logo} alt="" />
-              <div className="p-3">Menu</div>
+              <img className="img-fluid m-3" width="80px" src={Logo} alt="" />
+              <NavigationMenu />
             </div>
           </Col>
         </Row>
--- src/components/header/profile.jsx
+++ src/components/header/profile.jsx
@@ -1,18 +1,27 @@
 import { Button } from 'react-bootstrap'
 import React, { useState } from 'react'
-
+import PersonImage from '../../assets/images/person.jpg'
+import iranFlag from '../../assets/images/iran-flag.svg'
 
 
 const Profile = () => {
+
+    const[token, setToken] = useState(true)
 
     const loginHandler = () => {
         console.log('add to local storage');
     }
 
     return (
-        <>
-          <Button variant="primary" onClick={loginHandler}>ورود یا عضویت</Button>  
-        </>
+        token ? (
+            <div className="d-flex align-items-center">
+                <img className="rounded-circle" width="50px" height="50px" src={PersonImage} alt="person" />
+                <img className="rounded-circle" width="50px" height="50px" src={iranFlag} alt="person" />
+                <div className="flag"></div>
+            </div>
+        ) : (
+            <Button variant="primary" onClick={loginHandler}>ورود یا عضویت</Button>  
+        )
     )
 }
 
